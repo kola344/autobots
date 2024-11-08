c8ee1f3154696fcaf788bb44618aaba1530266af
kola <koli>
2024-11-08 19:08:53 +0300

    t

--- router.py
+++ router.py
@@ -4,12 +4,10 @@ from bots import connecting
 
 router = Router()
 
-@router.message(F.startswith('/connect'))
-async def process_message(message: Message):
-    token = message.text.split()[1]
-    await connecting(token, router)
-    await message.answer("Создание бота завершено")
-
 @router.message(F.text)
 async def process_messageeee(message: Message):
+    if '/connect' in message.text:
+        token = message.text.split()[1]
+        await connecting(token, router)
+        await message.answer("Создание бота завершено")
     await message.answer(message.text)
